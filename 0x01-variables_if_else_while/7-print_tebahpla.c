f548251f361b22a361c95521db0c0d2ccfe26c85
Boluwatife Oluwagbenga <tife2111@gmail.com>
2023-03-17 10:40:17 -0700

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char low;
+
+	for (low = 'z'; low <= 'a'; low++)
+		putchar(low);
+	printf("\n");
+	return (0);
+}
