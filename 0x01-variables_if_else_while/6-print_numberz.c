6a95274d0a5a7197d2287823c4ef14f41eecb010
Boluwatife Oluwagbenga <tife2111@gmail.com>
2023-03-17 10:29:24 -0700

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	putchar((char) i);
+	printf("\n");
+	return (0);
+}
