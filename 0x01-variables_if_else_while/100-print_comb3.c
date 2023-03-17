ee387646134e197654b8d2456af542340a90f810
Boluwatife Oluwagbenga <tife2111@gmail.com>
2023-03-17 15:30:43 -0700

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,18 +10,11 @@ int main(void)
 {
 	int d;
 
-	for (d = 0; d < 90; d++)
+	for (d = '0'; d < '10'; d++)
 	{
-		for ()
-		{
-			putchar((d / 10) + '0');
-			putchar((d % 10) + '0');
-		}
-		if (d != 89)
-		{
-			putchar(',');
-			putchar(' ');
-		}
+		putchar('0'+d);
+		putchar(',');
+		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
