edfe423c956b180a89a30d709782fdcc7347af90
Boluwatife Oluwagbenga <tife2111@gmail.com>
2023-03-21 13:57:31 -0700

    fnl

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int print_alphabet(void)
+{
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+		_putchar(n);
+
+	_putchar('\n');
+
+	return (0);
+}
