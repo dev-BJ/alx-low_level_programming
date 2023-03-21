291c623165aacdfa7609337cd47a8695a80ca506
Boluwatife Oluwagbenga <tife2111@gmail.com>
2023-03-21 13:38:41 -0700

    fnl

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,31 +1,4 @@
-#include <main.h>
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-
-kai-dg
-/
-holbertonschool-low_level_programming
-Public
-Code
-Issues
-4
-Pull requests
-Actions
-Projects
-Security
-Insights
-holbertonschool-low_level_programming/0x02-functions_nested_loops/0-holberton.c
-@kai-dg
-kai-dg changed to while loop
- 1 contributor
-Executable File  19 lines (17 sloc)  206 Bytes
-#include "holberton.h"
+#include "main.h"
 /**
  * main - Entry point
  *
